abc698b7c1448f4dcd1744ea7d915db5b1b11276
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-12-06 15:38:02 +0300

    style the teams section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import TeamsPage from './components/teamspage/TeamsPage'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
 function App() {
-  return <div className='App'>hello hackers!!</div>
+  return (
+    <div className='App'>
+      <TeamsPage />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/teamspage/assets/SliderData.js
@@ -0,0 +1,17 @@
+export const SliderData = [
+  {
+    image:
+      'https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&w=600',
+    name: 'john doe',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/614117/pexels-photo-614117.jpeg?auto=compress&cs=tinysrgb&w=600',
+    name: 'micheal sleighs',
+  },
+  {
+    image:
+      'https://images.pexels.com/photos/3861972/pexels-photo-3861972.jpeg?auto=compress&cs=tinysrgb&w=600',
+    name: 'sasha bailes',
+  },
+]
