a7bd36164ac374aedc57d790fdd67b19b888edb4
Gibson-Bett <gibubett@gmail.com>
2022-12-07 04:42:47 +0300

    add Article components and fetch function

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -1,7 +1,54 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Signup from '../signup/Signup';
+import Login from '../login/Login';
 
-const Article = () => {
-  return <div>artices page</div>
+
+
+function Article() {
+
+  const [loggedIn, setLoggedIn] = useState({})
+  const [articles, setArticles] = useState([])
+  const [post, setPosts] = useState(true)
+
+  const addNewUser = (newUser) => setLoggedIn(newUser);
+
+
+  useEffect(() => {
+    fetch(`http://localhost:`)
+    .then(r => r.json())
+    .then( (notes) => {
+      console.log(notes)
+      setArticles(notes)
+    })
+    }, [loggedIn, post])
+
+    const deleteArticle = (id) => {
+        const newArticles = articles.filter(note => note.id !== id)
+        setArticles(newArticles)
+    }
+
+    function handleNewPost() {
+      setPosts((post) => !post)
+    }
+
+
+
+  return (
+    <div className="App">
+      <Routes>
+          <Route path='/home' element= {
+          <Article articles={articles} 
+          loggedIn={loggedIn}
+           deleteArticle={deleteArticle} 
+           handleNewPost={handleNewPost}/>
+           }/> 
+          <Route path='/users/new' element = {<Signup addNewUser={addNewUser}/>}/>
+          <Route path='/login' element = { <Login setLoggedIn={setLoggedIn} />}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default Article
