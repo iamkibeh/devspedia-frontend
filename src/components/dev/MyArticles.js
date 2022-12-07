0517609e4873d7e234c3cae7181fefa9e4104d77
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-12-07 18:24:54 +0300

    update the dashboard, create the articles view

---
+++ src/components/dev/MyArticles.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import OneArticle from '../free-articles/OneArticle'
+
+const MyArticles = () => {
+  return (
+    <>
+      <h1>welcome to my articles</h1>
+      <div className='articles-container'>
+        <OneArticle />
+        <OneArticle />
+        <OneArticle />
+      </div>
+    </>
+  )
+}
+
+export default MyArticles
