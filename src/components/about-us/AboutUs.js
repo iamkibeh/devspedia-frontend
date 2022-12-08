59cdea54d7b8e4cc0a4cadac0274f77727c4a8f1
ronnykipkirui <ronnybet41@gmail.com>
2022-12-08 16:08:38 +0300

    git commit -m "DEV-32  add deleted items"

--- src/components/about-us/AboutUs.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const AboutUs = () => {
-  return <div>About us page</div>
-}
-
-export default AboutUs
--- src/components/articles/Article.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Article = () => {
-  return <div>artices page</div>
-}
-
-export default Article
