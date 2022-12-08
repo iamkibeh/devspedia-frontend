0020c52c932ce96630961bee7ef37123d0c79197
Gibson-Bett <gibubett@gmail.com>
2022-12-08 15:47:39 +0300

    fetch articles from backend server

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -1,25 +1,41 @@
-import React from 'react'
-import OneArticle from '../free-articles/OneArticle'
+// import React,{ useState,useEffect } from 'react'
+import React, {useState, useEffect} from "react"
+import { useInView } from 'react-intersection-observer'
 
-const Article = () => {
+
+function Article() {
+   
+    const { ref, inView } = useInView({
+      triggerOnce: true,
+      threshold: 0,
+      // rootMargin: '-200px'
+    })
+
+  const [articles, setArticles] = useState([])
+  useEffect(()=>{
+    fetch('/articles')
+    .then(r=>r.json())
+    .then(data=>{
+      console.log(data)
+      setArticles(data)
+    })
+  },[])
+
+const data=articles.map((article)=>(
+  <div className={inView ? 'articles-container' : null} ref={ref}>
+    <img src={article.featured_image} alt='Oops'/>
+    <h3 className='free-articles-title'>Title:{article.title}</h3>
+    <p>{article.content}</p>
+    <button className='show-more-button'>Likes:{article.likes}</button>
+  </div>
+))
+
   return (
-    <>
-      <div className='free-article-container'>
-        <div className='free-articles-title'>
-          <h2>Welcome to our articles pages</h2>
-        </div>
-        <div className='articles-container'>
-          <OneArticle />
-          <OneArticle />
-          <OneArticle />
-          <OneArticle />
-          <OneArticle />
-          <OneArticle />
-          <OneArticle />
-        </div>
-      </div>
-    </>
-  )
+    <div className='free-article-container'>
+      {data}
+{/* //<Article articles={console.log(articles)} /> */}
+    </div>
+  );
 }
 
 export default Article
