b4a34496469f6bd632d6e0301e3d330c9beb3aae
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-12-08 12:54:55 +0300

    complete articles section

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import OneArticle from '../free-articles/OneArticle'
 
 const Article = () => {
-  return <div>artices page</div>
+  return (
+    <>
+      <div className='free-article-container'>
+        <div className='free-articles-title'>
+          <h2>Welcome to our articles pages</h2>
+        </div>
+        <div className='articles-container'>
+          <OneArticle />
+          <OneArticle />
+          <OneArticle />
+          <OneArticle />
+          <OneArticle />
+          <OneArticle />
+          <OneArticle />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Article
