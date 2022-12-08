a53ef4b7e77cc226c4191c1aaec33f72e1f64266
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-12-08 21:59:00 +0300

    fix the bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ContactUs from '../contact-us/ContactUs'
 import FreeArticles from '../free-articles/FreeArticles'
 import HeroSection from '../hero-section/HeroSection'
 import TeamsPage from '../teamspage/TeamsPage'
 
 const Home = () => {
+  const [ourDevs, setOurDevs] = useState([])
+  const articles = []
+
+  useEffect(() => {
+    fetch('https://devspedia-api-production.up.railway.app/devs')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setOurDevs(data)
+      })
+  }, [])
+
+  for (let i = 0; i < ourDevs.length; i++) {
+    ourDevs[i].map((dev) => articles.push(dev))
+  }
+  const free = articles.filter((art) => art.id)
   return (
     <>
       <HeroSection />
