13faf047bf736ed0bda086309e363ac6bf1ece32
Gibson-Bett <gibubett@gmail.com>
2022-12-10 03:53:50 +0300

    set username to welcome message when user is logged in.

--- src/components/articles/Article.js
+++ src/components/articles/Article.js
@@ -8,28 +8,28 @@ function Article({ user, loggedInUsers, handleLogin }) {
   // const [loggedIn, setIsLoggedIn] = useState(false)
   console.log(loggedInUsers)
 
-  // useEffect(() => {
-  //   fetch('/articles')
-  //     .then((r) => r.json())
-  //     .then((data) => {
-  //       console.log(data)
-  //       setArticles(data)
-  //     })
-  // }, [])
-  // console.log(reactLocalStorage.getObject('users').success)
+  useEffect(() => {
+    fetch('/articles')
+      .then((r) => r.json())
+      .then((data) => {
+        console.log(data)
+        setArticles(data)
+      })
+  }, [])
+  console.log(reactLocalStorage.getObject('users').success)
 
   return (
     <>
       {reactLocalStorage.getObject('users').success ? (
         <div className='free-article-container'>
           <div className='free-articles-title'>
-            <h2>welcome to our articles</h2>
+            <h2> {reactLocalStorage.getObject('users').success}</h2>
           </div>
-          {/* <div className='articles-container'>
+          <div className='articles-container'>
         {
           articles.map(article => <OneArticle key={article.id} article = {article}/> )
         }
-      </div> */}
+      </div>
         </div>
       ) : (
         <>
