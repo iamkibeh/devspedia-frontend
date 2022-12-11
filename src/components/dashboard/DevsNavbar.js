083ebdbba4aae25527baf36ff0f08a92b29f425c
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-12-11 23:38:44 +0300

    complete creating the navbar

--- src/components/dashboard/DevsNavbar.js
+++ src/components/dashboard/DevsNavbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 
 const DevsNavbar = () => {
   const handleClick = () => {
-    window.location.href = '/'
+    Navigate('/')
     window.location.reload()
   }
   return (
